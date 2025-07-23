1dbcbc52be3f1855f1104bb5b12fe4b954b8adc8
Angelo-capy <angel_3127241051@utd.edu.mx>
2025-07-23 07:38:41 -0600

    Add files via upload

---
+++ P1/2_modulos_paquetes/utilizar_paquete.py
@@ -0,0 +1,8 @@
+from paquet1 import modulos
+
+print(modulos.saludos("Angel Jaimes"))
+
+nombre,tel=modulos.datos2()
+print(f"\n\tAgenda telefónica\n\t\tNombre: {nombre}\n\t\tTeléfono: {tel}")
+
+modulos.esperetecla()
